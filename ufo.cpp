21f0bad1e4a2740fe9c146d521950eebdaae4687
Jose Quiceno <divina.familia.nostri@gmail.com>
2025-02-08 10:21:11 -0600

    Check if letter is in codeword

--- ufo.cpp
+++ ufo.cpp
@@ -10,12 +10,23 @@ int main() {
     int misses = 0;
     std::vector<char> incorrect;
     bool guess = false;
+    char letter;
 
     // Game loop
     while (answer != codeword && misses < 7) {
         display_misses(misses);
 
         display_status(incorrect, answer);
+
+        std::cout << "Enter your letter guess: ";
+        std::cin >> letter;
+
+        for (int i = 0; i < codeword.size(); i++) {
+            if (letter == codeword[i]) {
+                answer[i] = letter;
+                guess = true;
+            }
+        }
 
         misses++;
     }
