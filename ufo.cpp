bf8bb078930e5e4ba45a40bec7098cd70aa151c5
Jose Quiceno <divina.familia.nostri@gmail.com>
2025-02-08 09:50:16 -0600

    Append win and lose conditions

--- ufo.cpp
+++ ufo.cpp
@@ -4,12 +4,21 @@
 int main() {
     greet();
 
-    // Game setup
+    // Set up variables
     std::string codeword = "codecademy";
     std::string answer = "__________";
     int misses = 0;
 
+    // Game loop
     while (answer != codeword && misses < 7) {
         misses++;
+    }
+
+    // Win and Lose conditions
+    if (answer == codeword) {
+        std::cout << "SUCCESS: Human has been saved and you earn a medal of honor!\n";
+    }
+    else {
+        std::cout << "FAILURE: The human will be experimented on. And it's your fault.\n";
     }
 }
