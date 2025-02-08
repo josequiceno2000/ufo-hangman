e1cae09704383af231c74bcff3ffe8dc6cfe278c
Jose Quiceno <divina.familia.nostri@gmail.com>
2025-02-08 10:05:06 -0600

    Display incorrect guesses each turn

--- ufo.cpp
+++ ufo.cpp
@@ -8,10 +8,18 @@ int main() {
     std::string codeword = "codecademy";
     std::string answer = "__________";
     int misses = 0;
+    std::vector<char> incorrect;
+    bool guess = false;
 
     // Game loop
     while (answer != codeword && misses < 7) {
         display_misses(misses);
+        std::cout << "Incorrect Guesses: ";
+
+        for (int i = 0; i < incorrect.size(); i++) {
+            std::cout << incorrect[i] << " ";
+        }
+
         misses++;
     }
 
