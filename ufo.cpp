a8ec12270a3a42918b8c7d3e855beeed163fb3d0
Jose Quiceno <divina.familia.nostri@gmail.com>
2025-02-08 09:47:24 -0600

    Add game loop

--- ufo.cpp
+++ ufo.cpp
@@ -8,4 +8,8 @@ int main() {
     std::string codeword = "codecademy";
     std::string answer = "__________";
     int misses = 0;
+
+    while (answer != codeword && misses < 7) {
+        misses++;
+    }
 }
